acdcac65c98911b45b91d2f3fb6a17c01fad65d1
MathiasPurwoko <mathiaspurwoko14@outlook.com>
2020-07-22 12:33:04 +0700

    React Hooks useState

--- latihanreact/src/App.js
+++ latihanreact/src/App.js
@@ -12,6 +12,11 @@ import DetailComp from './Component/Fungsional/DetailComp';
 import LisComp from './Component/Class/ListComp';
 import TambahComp from './Component/Class/Tambah';
 import EditComp from './Component/Class/EditComp';
+import KelasComp from './Component/Hooks/Class/KelasComp';
+import HooksComp from './Component/Hooks/Functional/HooksComp';
+// import BootstrapComp from './Component/Class/BootstrapComp';
+//import Parent from './Component/Class/Parent';
+// import logo from './logo.svg';
 //import Home from './Component/Fungsional/Home';
 //import Beranda from './Component/Class/Beranda';
 
@@ -27,6 +32,8 @@ const App = () => {
       <Route exact path="/mahasiswa" component={LisComp} />
       <Route exact path="/mahasiswa/tambah" component={TambahComp} />
       <Route exact path="/mahasiswa/edit" component={EditComp} />
+      <Route exact path="/kelas" component={KelasComp} />
+        <Route exact path="/hooks" component={HooksComp} />
        {/* <Route exact path="/detail/:id" component={DetailComp} /> */}
     </Switch>
 
